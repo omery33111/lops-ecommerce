5d3c5b60afa9c3231cb76af59d65e1f42cb7e4ff
Omer Yanai <omery33111@gmail.com>
2024-03-04 16:51:13 +0200

    finals

--- Front-end/shop/src/endpoints/endpoints.ts
+++ Front-end/shop/src/endpoints/endpoints.ts
@@ -4,14 +4,14 @@
 // ------------------------- SERVER ENDPOINT:
 // export const myServer = "http://127.0.0.1:8000";
 
-export const myServer = "https://ecommerce-lops.onrender.com";
+export const myServer = "https://lops-backend.onrender.com";
 
 
 
 // ------------------------- PRODUCTS ENDPOINTS:
 // export const productURL = "http://127.0.0.1:8000/product";
 
-export const productURL = "https://ecommerce-lops.onrender.com/product";
+export const productURL = "https://lops-backend.onrender.com/product";
 
 
 
@@ -19,48 +19,48 @@ export const productURL = "https://ecommerce-lops.onrender.com/product";
 // export const registerURL = "http://127.0.0.1:8000/authentication/register";
 // export const loginURL = "http://127.0.0.1:8000/authentication/login/";
 
-export const registerURL = "https://ecommerce-lops.onrender.com/authentication/register";
-export const loginURL = "https://ecommerce-lops.onrender.com/authentication/login/";
+export const registerURL = "https://lops-backend.onrender.com/authentication/register";
+export const loginURL = "https://lops-backend.onrender.com/authentication/login/";
 
 
 
 // ------------------------- PROFILE_USER ENDPOINTS:
 // export const profile_userURL = "http://127.0.0.1:8000/profile";
 
-export const profile_userURL = "https://ecommerce-lops.onrender.com/profile";
+export const profile_userURL = "https://lops-backend.onrender.com/profile";
 
 
 
 // ------------------------- SHIPPING ENDPOINTS:
 // export const shippingURL = "http://127.0.0.1:8000/shipping";
 
-export const shippingURL = "https://ecommerce-lops.onrender.com/shipping";
+export const shippingURL = "https://lops-backend.onrender.com/shipping";
 
 
 
 // ------------------------- CATEGORY ENDPOINTS:
 // export const categoryURL = "http://127.0.0.1:8000/category";
 
-export const categoryURL = "https://ecommerce-lops.onrender.com/category";
+export const categoryURL = "https://lops-backend.onrender.com/category";
 
 
 
 // ------------------------- CATEGORY ENDPOINTS:
 // export const reviewsURL = "http://127.0.0.1:8000/reviews";
 
-export const reviewsURL = "https://ecommerce-lops.onrender.com/reviews";
+export const reviewsURL = "https://lops-backend.onrender.com/reviews";
 
 
 
 // ------------------------- ORDER ENDPOINTS:
 // export const orderURL = "http://127.0.0.1:8000/order";
 
-export const orderURL = "https://ecommerce-lops.onrender.com/order";
+export const orderURL = "https://lops-backend.onrender.com/order";
 
 
 
 // ------------------------- ADMIN PANEL ENDPOINTS:
   // export const admin_panelURL = "http://127.0.0.1:8000/admin_panel";
 
-  export const admin_panelURL = "https://ecommerce-lops.onrender.com/admin_panel";
+  export const admin_panelURL = "https://lops-backend.onrender.com/admin_panel";
   
